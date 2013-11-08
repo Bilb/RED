2b362d3c881787a1c9480bdf5a96472cea7d3c54
daboooo <julien.uuuu@gmail.com>
2013-11-08 17:03:26 +0100

    hello

---
+++ src/utbm/lo54/projet/HelloWorld.java
@@ -0,0 +1,21 @@
+package utbm.lo54.projet;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+@Path("/hello")
+public class HelloWorld {
+
+    @GET //@Path("{myparam}")
+    @Produces("text/plain")
+    public String getHelloWorld() {
+    	return "helloWorld";
+    }
+}
+
+
+//public String getHtml(@PathParam("myparam") String myparam) {
+//return "<html><body>"+myparam+"</body></html>";
+//}
