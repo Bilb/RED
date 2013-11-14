463102ee7c74005c2886dd8086065a1da9dea5a1
daboooo <julien.uuuu@gmail.com>
2013-11-14 17:41:37 +0100

    Liste des Locations + recherche par Location

---
+++ src/utbm/lo54/projet/webservice/Locations.java
@@ -0,0 +1,60 @@
+package utbm.lo54.projet.webservice;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import utbm.lo54.projet.model.Location;
+
+@Path("/byLocation")
+public class Locations {
+
+	@GET 
+	@Produces("text/plain")
+	public String getLocations() {
+
+		Connection connexion = null;
+
+		try {
+			Class.forName("org.gjt.mm.mysql.Driver").newInstance();
+			String dataServerURL = new String("jdbc:mysql://localhost:3306/schoolFormation");
+			connexion = DriverManager.getConnection(dataServerURL,"blue","blue");
+			Statement statement = connexion.createStatement();
+			ResultSet resultSet = statement.executeQuery("select id, city from location");
+			ArrayList<Location> locations = new ArrayList<Location>();
+			
+			while (resultSet.next()) {
+				int id = resultSet.getInt("id");
+				String city = resultSet.getString("city");
+				Location location = new Location(id, city);
+				locations.add(location);
+				System.out.println("id = " + id + "	| city = " + city);
+			}
+		}
+		
+		catch (Exception e) {
+			System.out.println("Failed");
+			e.printStackTrace();
+
+		} 
+		
+		finally {
+			if(connexion !=null) {
+				try {
+					connexion.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return "ByLocation";
+	}
+}
