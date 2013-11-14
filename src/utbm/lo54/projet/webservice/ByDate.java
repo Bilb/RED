52319e02d28776925c853f448c3a831690a66585
Bilb <audric.bilb@gmail.com>
2013-11-14 18:06:54 +0100

    adding ByDate webservices, json not ok for now

--- src/utbm/lo54/projet/webservice/ByDate.java
+++ src/utbm/lo54/projet/webservice/ByDate.java
@@ -2,11 +2,19 @@ package utbm.lo54.projet.webservice;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
 
 
@@ -15,33 +23,57 @@ public class ByDate {
 
 
 	@GET 
-	@Produces("text/plain")
-	public String getHelloWorld() {
+	@Path("/")
+	@Produces(MediaType.APPLICATION_JSON)
+	public ByDateRecord getByDate(@QueryParam("date") String requestDate) {
 
-		Connection con = null;
-
+		//List<ByDateRecord> records = new ArrayList<ByDateRecord>();
 		try {
-			Class.forName("org.gjt.mm.mysql.Driver").newInstance();
-			String dataServerURL=new
-					String("jdbc:mysql://localhost:3306/schoolFormation");
-			con=DriverManager.getConnection(dataServerURL,"blue","blue");
+			DateFormat decodeFromQuery = new SimpleDateFormat("dd-MM-yyyy");
+			Date date = (Date) decodeFromQuery.parse(requestDate);
 
-			// do what to do
-		} catch (Exception e) {
-			System.out.println("Failed");
-			e.printStackTrace();
+			Connection con = null;
+			try {
+				Class.forName("org.gjt.mm.mysql.Driver").newInstance();
+				String dataServerURL=new
+						String("jdbc:mysql://localhost:3306/schoolFormation");
+				con=DriverManager.getConnection(dataServerURL,"blue","blue");
 
-		} finally {
-			if(con !=null) {
-				try {
-					con.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
+				PreparedStatement stmt = con.prepareStatement("SELECT ID, START, END, "
+						+ "COURSE_CODE, TITLE FROM COURSE_SESSION AS ses "
+						+ "INNER JOIN COURSE AS crs ON ses.COURSE_CODE=crs.CODE "
+						+ "WHERE START=?");
+
+				DateFormat encodeForSql = new SimpleDateFormat("yyyy-MM-dd");
+				stmt.setString(1, "" + encodeForSql.format(date));
+
+				ResultSet rs = stmt.executeQuery();
+
+				while(rs.next()) {
+					ByDateRecord record = new ByDateRecord(rs.getInt(1), rs.getString(2), rs.getString(3), 
+							rs.getString(4), rs.getString(5));
+					//records.add(record);
+					return record;
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+
+			} finally {
+				if(con !=null) {
+					try {
+						con.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
 				}
 			}
+
+		} catch (ParseException e) {
+			e.printStackTrace();
 		}
 
-		
-		return "byDate";
+		return null;
+
 	}
 }
---
+++ src/utbm/lo54/projet/webservice/ByDateRecord.java
@@ -0,0 +1,84 @@
+package utbm.lo54.projet.webservice;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@XmlRootElement
+public class ByDateRecord {
+
+	private int id;
+	private Date start;
+	private Date end;
+	private String courseCode;
+	private String title;
+
+
+
+	public ByDateRecord(int id, Date start, Date end, String courseCode,
+			String title) {
+		super();
+		this.id = id;
+		this.start = start;
+		this.end = end;
+		this.courseCode = courseCode;
+		this.title = title;
+	}
+
+	public ByDateRecord(int id, String start, String end, String courseCode,
+			String title) {
+		super();
+		this.id = id;
+		DateFormat encodeForSql = new SimpleDateFormat("yyyy-MM-dd");
+
+		try {
+			this.start = encodeForSql.parse(start);
+			this.end = encodeForSql.parse(end);
+		} catch (ParseException e) {
+			System.err.println("Incorrect DateFormat: must be : 'yyyy-MM-dd'");
+			e.printStackTrace();
+		}
+		this.courseCode = courseCode;
+		this.title = title;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getStart() {
+		return start;
+	}
+	public void setStart(Date start) {
+		this.start = start;
+	}
+	public Date getEnd() {
+		return end;
+	}
+	public void setEnd(Date end) {
+		this.end = end;
+	}
+	public String getCourseCode() {
+		return courseCode;
+	}
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+
+
+}
