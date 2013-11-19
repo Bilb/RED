2b68ff6ea6ee1bd98beff0c09b3f50c9d6cdcf8b
arktic <arktic17@gmail.com>
2013-11-19 17:04:58 +0100

    ByKeyword.java: now returning json correctly

--- src/utbm/lo54/projet/webservice/ByKeyword.java
+++ src/utbm/lo54/projet/webservice/ByKeyword.java
@@ -5,59 +5,64 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import utbm.lo54.projet.model.Course;
 
 @Path("/ByKeyword/{keyword}")
 public class ByKeyword {
 
-    @GET
-    @Produces("application/json")
-    public String getFormationList(@PathParam("keyword") String keyword) {
-    	
-    	StringBuilder formationContainingKeyword = new StringBuilder();
-    	formationContainingKeyword.append("Keyword:" + keyword);
-    	/* Chargement du driver JDBC pour MySQL */
-    	try {
-    	    Class.forName( "com.mysql.jdbc.Driver" );
-    	} catch ( ClassNotFoundException e ) {
-    		return "failed loading driver";
-    	}
-    	
-    	Connection connexion = null;
+	@GET
+	@Produces("application/json")
+	public String getFormationList(@PathParam("keyword") String keyword) {
+		/* loading the JDBC driver for MySQL */
 		try {
-    		    connexion = DriverManager.getConnection( "jdbc:mysql://localhost:3306/schoolFormation", "blue", "blue" );
-    		    Statement statement = connexion.createStatement();
-    		    
-    		    /* Exécution d'une requête de lecture */
-    		    ResultSet resultat = statement.executeQuery( "SELECT code, title  FROM Course WHERE title like '%"+ keyword +"%';" );
-    		    
-    		    //ObjectMapper mapper = new ObjectMapper();
-    		    /* Récupération des données du résultat de la requête de lecture */
-    		    while ( resultat.next() ) {
-    		    	//Course tempCourse = new Course(resultat.getString("code"), resultat.getString("title")) ;
-					//	RES = RES + mapper.writeValueAsString(tempCourse) + "\n";
-    		    	formationContainingKeyword.append("\nCode  = " + resultat.getString("code") + "\t\tTitle = " + resultat.getString("title"));
-    		    }
-    		    
-    		
-    		} catch ( SQLException e ) {
-    		    return "failed to connect";
-    		}
-    		    finally {
-    		    if ( connexion != null )
-    		        try {
-    		            /* Fermeture de la connexion */
-    		            connexion.close();
-    		        } catch ( SQLException ignore ) {
-    		            // Si une erreur survient lors de la fermeture, il suffit de l'ignorer
-    		        }
-    		}
-    	return formationContainingKeyword.toString();
-    }
+			Class.forName( "com.mysql.jdbc.Driver" );
+		} catch ( ClassNotFoundException e ) {
+			return "failed loading driver";
+		}
+
+		Connection connexion = null;
+		List<Course> courseListMatchingKeyword = new ArrayList<Course>();
+		
+		try {
+			connexion = DriverManager.getConnection( "jdbc:mysql://localhost:3306/schoolFormation", "blue", "blue" );
+			Statement statement = connexion.createStatement();
+			ResultSet resultat = statement.executeQuery( "SELECT code, title  FROM Course WHERE title like '%"+ keyword +"%';" );
+			while ( resultat.next() ) {
+				Course record = new Course(resultat.getString("code"), resultat.getString("title"));
+				courseListMatchingKeyword.add(record);
+			}
+
+		} catch ( SQLException e ) {
+			return "failed to connect";
+		}
+		finally {
+			if ( connexion != null ) {
+				try {
+					connexion.close();
+				} catch ( SQLException e ) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		// Converting to json format
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			return mapper.writeValueAsString(courseListMatchingKeyword);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		return "[ ]";
+	}
 }
