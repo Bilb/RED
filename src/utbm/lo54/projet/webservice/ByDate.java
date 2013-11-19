7bdfbdfe063eff542a9abd753efd61ed44cc6f09
Bilb <audric.bilb@gmail.com>
2013-11-19 17:33:03 +0100

    add sql connection from datasources to ByDate

--- src/utbm/lo54/projet/webservice/ByDate.java
+++ src/utbm/lo54/projet/webservice/ByDate.java
@@ -1,7 +1,6 @@
 package utbm.lo54.projet.webservice;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,6 +11,10 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -35,55 +38,46 @@ public class ByDate {
 	@Path("/")
 	@Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
 	public String getByDate(@QueryParam("date") String requestDate) {
-		
+
 		IndentObjectMapperProvider provider = new IndentObjectMapperProvider();
 		ObjectMapper mapper = provider.getContext(null);
-		
+
 
 		List<ByDateRecord> records = new ArrayList<ByDateRecord>();
+		DateFormat decodeFromQuery = new SimpleDateFormat("dd-MM-yyyy");
+		
+
 		try {
-			DateFormat decodeFromQuery = new SimpleDateFormat("dd-MM-yyyy");
 			Date date = (Date) decodeFromQuery.parse(requestDate);
+			Context namingContext = new InitialContext();
 
-			Connection con = null;
-			try {
-				Class.forName(Commons.JDBC_DRIVER).newInstance();
-				String dataServerURL=new
-						String(Commons.SQL_SERVER_URL);
-				con=DriverManager.getConnection(dataServerURL,Commons.DATABASE_USER, Commons.DATABASE_PASSWORD);
+			DataSource dataSource = (DataSource)namingContext.lookup("java:comp/env/jdbc/schoolFormationDataSource");
+			Connection con = dataSource.getConnection();
 
-				PreparedStatement stmt = con.prepareStatement("SELECT ID, START, END, "
-						+ "COURSE_CODE, TITLE FROM COURSE_SESSION AS ses "
-						+ "INNER JOIN COURSE AS crs ON ses.COURSE_CODE=crs.CODE "
-						+ "WHERE START=?");
+			PreparedStatement stmt = con.prepareStatement("SELECT ID, START, END, "
+					+ "COURSE_CODE, TITLE FROM COURSE_SESSION AS ses "
+					+ "INNER JOIN COURSE AS crs ON ses.COURSE_CODE=crs.CODE "
+					+ "WHERE START=?");
 
-				DateFormat encodeForSql = new SimpleDateFormat("yyyy-MM-dd");
-				stmt.setString(1, "" + encodeForSql.format(date));
+			DateFormat encodeForSql = new SimpleDateFormat("yyyy-MM-dd");
+			stmt.setString(1, "" + encodeForSql.format(date));
 
-				ResultSet rs = stmt.executeQuery();
-				while(rs.next()) {
-					ByDateRecord record = new ByDateRecord(rs.getInt(1), rs.getString(2), 
-							rs.getString(3), rs.getString(4), rs.getString(5));
-					records.add(record);
-				}
-
-
-			} catch (Exception e) {
-				e.printStackTrace();
-
-			} finally {
-				if(con !=null) {
-					try {
-						con.close();
-					} catch (SQLException e) {
-						e.printStackTrace();
-					}
-				}
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()) {
+				ByDateRecord record = new ByDateRecord(rs.getInt(1), rs.getString(2), 
+						rs.getString(3), rs.getString(4), rs.getString(5));
+				records.add(record);
 			}
 
-		} catch (ParseException e) {
+		} catch (NamingException e1) {
+			e1.printStackTrace();
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		catch (ParseException e) {
+			e.printStackTrace();
+		}
+
 
 		try {
 			return mapper.writeValueAsString(records);
